5dfd6c1aa3054ffbb49a44c2cb081abb23ca7d2d
suvarnalakshmi123 <150534753+suvarnalakshmi123@users.noreply.github.com>
2024-03-18 16:48:35 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+plant=input()
+if plant=="spathiphyllum":
+   print("No, I want a big Spathiphyllum!")
+elif plant=="pelargonium":
+   print("Spathiphyllum! Not pelargonium!")
+elif plant=="Spathiphyllum":
+   print("Yes - Spathiphyllum is the best plant ever!")
+else : print("Spathiphyllum! Not",plant)
+   
