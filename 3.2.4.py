5ee175bd0a05e3459ccd6e56f251b582c9384bce
suvarnalakshmi123 <150534753+suvarnalakshmi123@users.noreply.github.com>
2024-03-24 15:32:03 +0530

    Add files via upload

---
+++ 3.2.4.py
@@ -0,0 +1,7 @@
+secret_number = 777
+number=int(input("enter the secret number="))
+while number != secret_number :
+ print("Ha ha! You're stuck in my loop!")
+ number=int(input())
+else:
+ print("Well done, muggle! You are free now.")
